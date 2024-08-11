8aa02cc21dc1fca08fe9a68aa29aff9f98dbc944
Arkajyoti Das <develop@arkajyotidas.com>
2024-08-11 22:44:46 +0530

    updated the automation codes

--- automation.py
+++ automation.py
@@ -46,6 +46,10 @@ for i in range(tindie_total_orders_pages):
         tindie_order_data_order_amount_tindie_charge = tindie_order_data["orders"][tindie_order_count-1]["total_tindiefee"]
         tindie_order_data_order_amount_received = tindie_order_data["orders"][tindie_order_count-1]["total_seller"]
         tindie_order_data_order_tracking_code = tindie_order_data["orders"][tindie_order_count-1]["tracking_code"]
+        if tindie_order_data["orders"][tindie_order_count-1]["payment"]== "complete": 
+            tindie_order_data_order_status = "completed"
+        else:
+            tindie_order_data_order_status = "new"
 
         tindie_search_statement = ("select * from tindie_orders where order_number = %(tindie_order_number)s")
         tindie_db_cursor.execute(tindie_search_statement,{'tindie_order_number' : tindie_order_data_order_number})
@@ -55,7 +59,7 @@ for i in range(tindie_total_orders_pages):
             tindie_db_insert_stmt_col = ("insert into tindie_orders (order_number,order_amount_total,order_amount_cc_charge,"
                                          "order_amount_tindie_charge,order_amount_received,order_placed_date,order_shipped_date,customer_company_name,"
                                          "customer_email,customer_contact,customer_name,shipping_country,shipping_state,shipping_city,shipping_street,"
-                                         "shipping_postcode,order_shipping_status,order_tracking_code) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)") 
+                                         "shipping_postcode,order_shipping_status,order_tracking_code,order_status) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)") 
             tindie_db_insert_stmt_values = (tindie_order_data_order_number,tindie_order_data_order_amount_total,
                                             tindie_order_data_order_amount_cc_charge,tindie_order_data_order_amount_tindie_charge,
                                             tindie_order_data_order_amount_received,tindie_order_data_order_placed_date,tindie_order_data_order_shipped_date,
@@ -63,8 +67,8 @@ for i in range(tindie_total_orders_pages):
                                             tindie_order_data_customer_contact,tindie_order_data_customer_name,
                                             tindie_order_data_shipping_country,tindie_order_data_shipping_state,
                                             tindie_order_data_shipping_city,tindie_order_data_shipping_street,
-                                            tindie_order_data_shipping_postcode,tindie_order_data_order_shipping_status,tindie_order_data_order_tracking_code
-                                            )
+                                            tindie_order_data_shipping_postcode,tindie_order_data_order_shipping_status,tindie_order_data_order_tracking_code,
+                                            tindie_order_data_order_status)
             tindie_db_cursor.execute(tindie_db_insert_stmt_col,tindie_db_insert_stmt_values)
             tindie_db.commit()
         else:
